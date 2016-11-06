f016ba49e9307ea2b5cfff50a0a15b1989056d33
Adrian Sampson <adrian@radbox.org>
2016-11-06 16:55:53 -0600

    Include a few more details on the config

--- flatten.py
+++ flatten.py
@@ -15,6 +15,55 @@ APPS = [
     "LFW & CASIA, OpenFace",
 ]
 
+CONFIG_INFO = {
+    "V0": {
+        "only": "(original)",
+        "skip": "(original)",
+        "special": "(original)",
+    },
+    "V1": {
+        "only": "(full reverse)",
+        "skip": "(full reverse)",
+        "special": "(full reverse)",
+    },
+    "V2": {
+        "only": "tone map",
+    },
+    "V3": {
+        "only": "gamut map",
+    },
+    "V4": {
+        "only": "transform",
+    },
+    "V5": {
+        "only": "denoise",
+    },
+    "V6": {
+        "only": "demosaic",
+    },
+    "V7": {
+        "skip": "tone map",
+    },
+    "V8": {
+        "skip": "gamut map",
+    },
+    "V9": {
+        "skip": "transform",
+    },
+    "V10": {
+        "skip": "denoise",
+    },
+    "V11": {
+        "skip": "demosaic",
+    },
+    "V12": {
+        "special": "demosaic + denoise + tone map",
+    },
+    "V13": {
+        "special": "demosaic + tone map",
+    },
+}
+
 
 def all_runs(infn):
     """Given an input CSV in Mark's format, generate data tuples for
@@ -33,14 +82,44 @@ def all_runs(infn):
                 yield name, stages, interp, app, row[app]
 
 
+def _union_all(iterables):
+    """Return a set representing the union of all the contents of an
+    iterable of iterables.
+    """
+    out = set()
+    for iterable in iterables:
+        out.update(iterable)
+    return out
+
+
 def flatten(infn, outfn):
     """Given a compact CSV, write a new CSV with one result per line.
     """
+    # Find all the new keys from CONFIG_INFO.
+    config_keys = list(_union_all(CONFIG_INFO.values()))
+
+    # Translate the data.
     with open(outfn, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(('name', 'stages', 'interp', 'app', 'error'))
+        # Get the set of keys for the columns.
+        header = ['name', 'stages', 'interp', 'app', 'error']
+        header += config_keys
+
+        # Write the header row.
+        writer = csv.DictWriter(f, header)
+        writer.writeheader()
+
+        # Write each data row.
         for name, stages, interp, app, score in all_runs(infn):
-            writer.writerow((name, stages, interp, app, score))
+            row = {
+                'name': name,
+                'stages': stages,
+                'interp': interp,
+                'app': app,
+                'error': score,
+            }
+            if name in CONFIG_INFO:
+                row.update(CONFIG_INFO[name])
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
