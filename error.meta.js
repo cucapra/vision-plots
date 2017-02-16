0a749a5f7308953ed1f8f654f26df80d90d9d8cc
Adrian Sampson <adrian@radbox.org>
2017-02-16 18:59:47 -0500

    Add "bars" parameter

--- error.meta.js
+++ error.meta.js
@@ -1,11 +1,21 @@
 {
   "data": { "url": "final.csv" },
-  "mark": "point",
+  "mark": $.bars ? "bar" : "point",
   "encoding": {
-    "x": {"field": $.category, "type": "nominal"},
+    "x": $.bars ? {"field": "app", "type": "nominal", "axis": false}
+          : {"field": $.category, "type": "nominal"},
     "y": {"field": $.norm ? "error_norm" : "error", "type": "quantitative"},
-    "color": {"field": "app", "type": "nominal"}
+    "color": {"field": "app", "type": "nominal"},
+    "column": $.bars ? {
+      "field": $.category, "type": "nominal",
+      "scale": {"padding": 4},
+      "axis": {"orient": "bottom"},
+    } : undefined,
   },
+
+  "config": $.bars ? {
+    "facet": {"cell": {"strokeWidth": 0}}
+  } : undefined,
 
   // Include only values belonging to the category.
   "transform": { "filter": "!!datum." + $.category },
