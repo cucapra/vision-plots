01372d83ef60e800c7ef28bc5396ef80107b7dda
Adrian Sampson <adrian@radbox.org>
2017-02-24 17:43:03 -0500

    Use layers for the single-app plot

--- single.meta.js
+++ single.meta.js
@@ -1,24 +1,28 @@
 {
   "data": { "url": "final.csv" },
-  "mark": "bar",
-  "encoding": {
-    "x": {
-      "field": $.category, "type": "nominal",
-      "scale": {"bandSize": 10},
+  "layers": [
+    {
+      "mark": "bar",
+      "encoding": {
+        "x": {
+          "field": $.category, "type": "nominal",
+          "scale": {"bandSize": 10},
 
-      // Label each plot with the app. It is ugly to use the x-axis title for
-      // this, but: https://github.com/vega/vega-lite/issues/431
-      "title": $.app,
-    },
-    "y": {
-      "field": "error", "type": "quantitative",
+          // Label each plot with the app. It is ugly to use the x-axis title
+          // for this, but: https://github.com/vega/vega-lite/issues/431
+          "title": $.app,
+        },
+        "y": {
+          "field": "error", "type": "quantitative",
 
-      // A hack! We add the vertical axis title only on the left-most plot,
-      // which is the LeNet3 app.
-      "title": $.app === "LeNet3" ? "error" : "",
+          // A hack! We add the vertical axis title only on the left-most plot,
+          // which is the LeNet3 app.
+          "title": $.app === "LeNet3" ? "error" : "",
+        },
+        "color": {"field": $.category, "type": "nominal", "legend": false},
+      },
     },
-    "color": {"field": $.category, "type": "nominal", "legend": false},
-  },
+  ],
 
   // Include only values belonging to the category and the current app.
   "transform": {
