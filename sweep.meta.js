97f785a4b8f2d032b1990d09e768201a6aabb81e
Adrian Sampson <adrian@radbox.org>
2017-03-04 21:17:43 -0500

    Sweep plot: distinguish apps by color

--- sweep.meta.js
+++ sweep.meta.js
@@ -2,8 +2,19 @@
   "data": { "url": "final.csv" },
   "mark": "line",
   "encoding": {
-    "x": {"field": $.category, "type": "quantitative"},
-    "y": {"field": $.norm ? "error_norm" : "error", "type": "quantitative"},
+    "x": {
+      "field": $.category, "type": "quantitative",
+      "axis": {"title": "bits"},
+    },
+    "y": {
+      "field": $.norm ? "error_norm" : "error",
+      "type": "quantitative",
+      "axis": {"title": $.norm ? "normalized error" : "error"},
+    },
+    "color": {
+      "field": "app", "type": "nominal",
+      "axis": {"title": "Application"},
+    },
   },
 
   // Include only values belonging to the category.
